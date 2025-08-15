af49f90bc401460914df7405400e8c825e0e9f1f
Haris Aftab <aftabharis91@gmail.com>
2025-08-16 04:33:26 +0500

    asd

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,33 +10,33 @@
         <meta name="description" content="@yield('meta_description', 'saas, software, tools, solutions')">
         <title>@yield('title', 'Service Site')</title>
         <!-- Favicon -->
-        <link rel="apple-touch-icon" href="web-assets/images/fav.svg" />
-        <link rel="shortcut icon" type="image/x-icon" href="web-assets/images/fav.svg" />
+        <link rel="apple-touch-icon" href="{{ asset('web-assets/images/fav.svg') }}" />
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('web-assets/images/fav.svg') }}" />
 
         <!-- Bootstrap  v5.1.3 css -->
-        <link rel="stylesheet" href="web-assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/bootstrap.min.css') }}" />
         <!-- Meanmenu  css -->
-        <link rel="stylesheet" href="web-assets/css/meanmenu.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/meanmenu.css') }}" />
         <!-- Sal css -->
-        <link rel="stylesheet" href="web-assets/css/sal.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/sal.css') }}" />
         <!-- Magnific css -->
-        <link rel="stylesheet" href="web-assets/css/magnific-popup.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/magnific-popup.css') }}" />
         <!-- Swiper Slider css -->
-        <link rel="stylesheet" href="web-assets/css/swiper.min.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/swiper.min.css') }}" />
         <!-- Carousel css file -->
-        <link rel="stylesheet" href="web-assets/css/owl.carousel.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/owl.carousel.css') }}" />
         <!-- Icons css -->
-        <link rel="stylesheet" href="web-assets/css/icons.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/icons.css') }}" />
         <!-- Odometer css -->
-        <link rel="stylesheet" href="web-assets/css/odometer.min.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/odometer.min.css') }}" />
         <!-- Select css -->
-        <link rel="stylesheet" href="web-assets/css/nice-select.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/nice-select.css') }}" />
         <!-- Animate css -->
-        <link rel="stylesheet" href="web-assets/css/animate.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/animate.css') }}" />
         <!-- Style css -->
-        <link rel="stylesheet" href="web-assets/css/style.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/style.css') }}" />
         <!-- Responsive css -->
-        <link rel="stylesheet" href="web-assets/css/responsive.css" />
+        <link rel="stylesheet" href="{{ asset('web-assets/css/responsive.css') }}" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -52,33 +52,33 @@
         @include('partials.site.footer')
       
         <!-- Modernizr.JS -->
-        <script src="web-assets/js/modernizr-2.8.3.min.js"></script>
+        <script src="{{ asset('web-assets/js/modernizr-2.8.3.min.js') }}"></script>
         <!-- jQuery.min JS -->
-        <script src="web-assets/js/jquery.min.js"></script>
+        <script src="{{ asset('web-assets/js/jquery.min.js') }}"></script>
         <!-- Bootstrap.min JS -->
-        <script src="web-assets/js/bootstrap.min.js"></script>
+        <script src="{{ asset('web-assets/js/bootstrap.min.js') }}"></script>
         <!-- Meanmenu JS -->
-        <script src="web-assets/js/meanmenu.js"></script>
+        <script src="{{ asset('web-assets/js/meanmenu.js') }}"></script>
         <!-- Imagesloaded JS -->
-        <script src="web-assets/js/imagesloaded.pkgd.min.js"></script>
+        <script src="{{ asset('web-assets/js/imagesloaded.pkgd.min.js') }}"></script>
         <!-- Isotope JS -->
-        <script src="web-assets/js/isotope.pkgd.min.js"></script>
+        <script src="{{ asset('web-assets/js/isotope.pkgd.min.js') }}"></script>
         <!-- Magnific JS -->
-        <script src="web-assets/js/jquery.magnific-popup.min.js"></script>
+        <script src="{{ asset('web-assets/js/jquery.magnific-popup.min.js') }}"></script>
         <!-- Swiper.min JS -->
-        <script src="web-assets/js/swiper.min.js"></script>
+        <script src="{{ asset('web-assets/js/swiper.min.js') }}"></script>
         <!-- Owl.min JS -->
-        <script src="web-assets/js/owl.carousel.js"></script>
+        <script src="{{ asset('web-assets/js/owl.carousel.js') }}"></script>
         <!-- Appear JS -->
-        <script src="web-assets/js/jquery.appear.min.js"></script>
+        <script src="{{ asset('web-assets/js/jquery.appear.min.js') }}"></script>
         <!-- Odometer JS -->
-        <script src="web-assets/js/odometer.min.js"></script>
+        <script src="{{ asset('web-assets/js/odometer.min.js') }}"></script>
         <!-- Sal JS -->
-        <script src="web-assets/js/sal.js"></script>
+        <script src="{{ asset('web-assets/js/sal.js') }}"></script>
         <!-- Nice JS -->
-        <script src="web-assets/js/jquery.nice-select.min.js"></script>
+        <script src="{{ asset('web-assets/js/jquery.nice-select.min.js') }}"></script>
         <!-- Main JS -->
-        <script src="web-assets/js/main.js"></script>
-<script src="web-assets/js/extra.js"></script>
+        <script src="{{ asset('web-assets/js/main.js') }}"></script>
+<script src="{{ asset('web-assets/js/extra.js') }}"></script>
     </body>
 </html>
