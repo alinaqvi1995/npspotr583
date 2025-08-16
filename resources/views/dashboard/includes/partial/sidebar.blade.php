6c192388205d0609e36cb59c9e2c91a7bd2c9822
alinaqvi1995 <alinaqvif@gmail.com>
2025-08-16 20:39:19 +0500

    s

--- resources/views/dashboard/includes/partial/sidebar.blade.php
+++ resources/views/dashboard/includes/partial/sidebar.blade.php
@@ -2,10 +2,10 @@
 <aside class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div class="logo-icon">
-            <img src="{{ asset('admin/images/logo-icon.png') }}" class="logo-img" alt="Logo">
+            {{-- <img src="{{ asset('admin/images/logo-icon.png') }}" class="logo-img" alt="Logo"> --}}
         </div>
         <div class="logo-name flex-grow-1">
-            <h5 class="mb-0">Maxton</h5>
+            <h5 class="mb-0">Bridgeway</h5>
         </div>
         <div class="sidebar-close">
             <span class="material-icons-outlined">close</span>
