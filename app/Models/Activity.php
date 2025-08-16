8386c730f8103d9b03361b85d315b4758e603721
alinaqvi1995 <alinaqvif@gmail.com>
2025-08-16 22:55:54 +0500

    s

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Spatie\Activitylog\Models\Activity as SpatieActivity;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
-class Activity extends SpatieActivity
+class Activity extends Model
 {
     /**
      * Casts for JSON / datetime columns.
