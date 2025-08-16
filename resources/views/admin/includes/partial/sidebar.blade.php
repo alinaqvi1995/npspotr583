0a71d888deb8d814fdb4ae6cda9b86d173218ac6
Haris Aftab <aftabharis91@gmail.com>
2025-08-16 06:10:54 +0500

    as

--- resources/views/admin/includes/partial/sidebar.blade.php
+++ resources/views/admin/includes/partial/sidebar.blade.php
@@ -14,15 +14,15 @@
     <div class="sidebar-nav">
         <ul class="metismenu" id="sidenav">
             <li>
-                <a href="{{ route('dashboard') }}" class="has-arrow">
+                <a href="{{ route('dashboard') }}" class="">
                     <div class="parent-icon"><i class="material-icons-outlined">home</i></div>
                     <div class="menu-title">Dashboard</div>
                 </a>
             </li>
-            <li class="menu-label">UI Elements</li>
+            <li class="menu-label">Elements</li>
             <li>
                 <a href="{{ route('categories.index') }}">
-                    <div class="parent-icon"><i class="material-icons-outlined">category</i></div>
+                    <div class="parent-icon"><i class="material-icons-outlined">Category</i></div>
                     <div class="menu-title">Categories</div>
                 </a>
             </li>
@@ -36,6 +36,90 @@
                 <a href="{{ route('admin.quotes.index') }}">
                     <div class="parent-icon"><i class="material-icons-outlined">request_quote</i></div>
                     <div class="menu-title">Quotes</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Asking</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Interested</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Follow Up</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Not Interested</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">No Respons</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Booked</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Payment Missing</div>
+                </a>
+            </li>
+                        <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Listed</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Dispatch</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Pickup</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Delivery</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Completed</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Deleted</div>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <div class="parent-icon"><i class="material-icons-outlined"></i></div>
+                    <div class="menu-title">Canceled</div>
                 </a>
             </li>
         </ul>
