30e92ea63ddb5212b1039ae1191947bf01b91ba3
Haris Aftab <aftabharis91@gmail.com>
2025-08-21 19:05:06 +0500

    asd

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -139,44 +139,4 @@
                 </div>
             </section>
         <!--========== Contact Page End ==============-->
-
-        <!--=========== Newsletter Section Start =========-->
-        <section class="tj-subscribe-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div
-                            class="subscribe-content-box d-flex align-items-center justify-content-between"
-                            data-bg-image="web-assets/images/banner/subscribe.png"
-                        >
-                            <div class="subscribe-content d-flex align-items-center">
-                                <div class="mail-icon">
-                                    <img src="web-assets/images/icon/email.svg" alt="Icon" />
-                                </div>
-                                <div class="subscribe-title">
-                                    <h3 class="title">Subscribe Our Newslatter</h3>
-                                </div>
-                            </div>
-                            <div class="subscribe-form d-flex align-items-center">
-                                <div class="subscribe-input">
-                                    <input
-                                        type="text"
-                                        id="email"
-                                        name="emailAddress"
-                                        placeholder="Email Address"
-                                        required=""
-                                    />
-                                </div>
-                                <div class="tj-theme-button">
-                                    <button class="tj-submit-btn" type="submit" value="submit">
-                                        Submit Now <i class="fa-light fa-arrow-right"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--=========== Newsletter Section End =========-->
 @endsection
