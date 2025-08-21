392f911fe817036e78fa582d2ccc21e03e940b04
alinaqvi1995 <alinaqvif@gmail.com>
2025-08-22 00:54:49 +0500

    s

--- app/Models/VehicleMakeModel.php
+++ app/Models/VehicleMakeModel.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class VehicleMakeModel extends Model
 {
     protected $table = 'wp_vehiclelisting';
-    protected $primaryKey = 'vehicleid';
     public $timestamps = false;
 
     protected $fillable = [
