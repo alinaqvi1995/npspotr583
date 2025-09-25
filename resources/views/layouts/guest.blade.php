38284d81341a8c4620a88d2a1193ff75201c8c40
Haris Aftab <aftabharis91@gmail.com>
2025-09-25 22:52:36 +0500

    df

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,16 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-    {{-- <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="theme-style-mode" content="1">
-    <meta name="description" content="@yield('meta_description', 'saas, software, tools, solutions')">
-    <title>@yield('title', 'Service Site')</title>
-    <!-- Favicon --> --}}
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -28,19 +18,6 @@
 
     <!-- Canonical -->
     <link rel="canonical" href="{{ url()->current() }}">
-
-    <!-- Open Graph (Facebook, LinkedIn) -->
-    <meta property="og:title" content="@yield('title', 'Bridgeway Logistics LLC')">
-    <meta property="og:description" content="@yield('meta_description', 'From open transport to heavy equipment shipping, we offer a full range of tailored logistics services.')">
-    <meta property="og:image" content="{{ asset('web-assets/images/og-image.jpg') }}">
-    <meta property="og:url" content="{{ url()->current() }}">
-    <meta property="og:type" content="website">
-
-    <!-- Twitter Card -->
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:title" content="@yield('title', 'Bridgeway Logistics LLC')">
-    <meta name="twitter:description" content="@yield('meta_description', 'From open transport to heavy equipment shipping, we offer a full range of tailored logistics services.')">
-    <meta name="twitter:image" content="{{ asset('web-assets/images/og-image.jpg') }}">
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" href="{{ asset('web-assets/images/fav.svg') }}" />
