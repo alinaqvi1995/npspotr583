4e078daff4e38f58421f4f47de84e8bd35cd863c
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-10-31 07:09:57 +0500

    s

--- app/Services/QuoteService.php
+++ app/Services/QuoteService.php
@@ -19,7 +19,7 @@ class QuoteService
             ->orderBy('created_at', 'desc');
 
         if (Auth::user()->email == 'Huzaifa@gmail.com') {
-            dd($requestedStatus, $query->get()->toArray());
+            dd($requestedStatus);
             // dd($query->where('id', 'like', "%{$search}%")->get()->toArray());
         }
 
