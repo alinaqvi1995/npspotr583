cfc4f998adcc4bbf4ac826d982fa17586b7f08e2
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2025-12-22 19:19:38 +0500

    s

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -353,7 +353,7 @@
                 <div class="card-body">
                     <div class="d-flex align-items-start justify-content-between mb-3">
                         <h5 class="mb-0">Recent Activity</h5>
-                        <a href="{{ route('dashboard.quotes.index') }}" class="btn btn-sm btn-outline-primary">
+                        <a href="{{ route('dashboard.quotes.index', 'New') }}" class="btn btn-sm btn-outline-primary">
                             View All Quotes
                         </a>
                     </div>
