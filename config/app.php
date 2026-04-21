84b83643e692fde95f37cd3b510c1f007b66cf41
Ali Naqvi <alinaqvif@gmail.com>
2026-04-21 12:25:16 +0500

    s

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://bridgewaylogisticsllc.com'),
+    'url' => env('APP_URL', 'https://www.bridgewaylogisticsllc.com'),
 
     /*
     |--------------------------------------------------------------------------
