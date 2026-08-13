ef826d9eb516559ba7b1963de0b00824c5cbdc14
Haris Aftab <aftabharis91@gmail.com>
2026-08-14 01:57:13 +0500

    ss

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -4,100 +4,172 @@
 
 @section('content')
 
-```html
+```html id="f5p8xz"
 <!--========== breadcrumb Start ==============-->
-<section class="breadcrumb-wrapper" data-bg-image="{{ asset('web-assets/images/banner/cta-bg.webp') }}">
+<section class="breadcrumb-wrapper"
+    data-bg-image="{{ asset('web-assets/images/banner/cta-bg.webp') }}">
+
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
+
                 <div class="breadcrumb-content">
-                    <h1 class="breadcrumb-title text-center">Contact Us</h1>
+
+                    <h1 class="breadcrumb-title text-center">
+                        Contact Us
+                    </h1>
+
                     <div class="breadcrumb-link">
+
                         <span>
-                            <a href="#">
+                            <a href="{{ url('/') }}">
                                 <span>Home</span>
                             </a>
                         </span>
+
                         >
+
                         <span>
-                            <span> Contact</span>
+                            <span>Contact</span>
                         </span>
+
                     </div>
+
                 </div>
+
             </div>
         </div>
     </div>
+
 </section>
 <!--========== breadcrumb End ==============-->
 
 
 <!--========== Contact Page Start ==============-->
 <section class="tj-contact-page">
+
     <div class="container">
 
+        <!-- Contact Information -->
         <div class="row tj-contact-box">
 
+            <!-- Phone -->
             <div class="col-lg-4 col-md-6">
+
                 <div class="tj-contact-list">
+
                     <div class="contact-icon">
                         <i class="flaticon-phone-call"></i>
                     </div>
+
                     <div class="contact-header">
-                        <span>Any Questions? Call us</span>
-                        <a href="tel:+17134706157">+1 (713) 470-6157</a>
+
+                        <span>
+                            Any Questions? Call us
+                        </span>
+
+                        <a href="tel:+17134706157">
+                            +1 (713) 470-6157
+                        </a>
+
                     </div>
+
                 </div>
+
             </div>
 
+
+            <!-- Email -->
             <div class="col-lg-4 col-md-6">
+
                 <div class="tj-contact-list">
+
                     <div class="contact-icon">
                         <i class="flaticon-email-3"></i>
                     </div>
+
                     <div class="contact-header">
-                        <span>Any Questions? Email us</span>
+
+                        <span>
+                            Any Questions? Email us
+                        </span>
+
                         <a href="mailto:info@bridgewaylogistics.com">
                             info@bridgewaylogistics.com
                         </a>
+
                     </div>
+
                 </div>
+
             </div>
 
+
+            <!-- Address -->
             <div class="col-lg-4 col-md-6">
+
                 <div class="tj-contact-list">
+
                     <div class="contact-icon">
                         <i class="flaticon-map"></i>
                     </div>
+
                     <div class="contact-header">
-                        <span>5402 Renwick Dr Apt 902</span>
-                        <a href="#">Houston, TX 77081, United States</a>
+
+                        <span>
+                            5402 Renwick Dr Apt 902
+                        </span>
+
+                        <a href="#">
+                            Houston, TX 77081, United States
+                        </a>
+
                     </div>
+
                 </div>
+
             </div>
 
         </div>
 
 
+        <!-- Contact Form + Map -->
         <div class="row align-items-center">
 
+            <!-- Contact Form -->
             <div class="col-lg-7">
 
                 <div class="tj-section-heading">
-                    <span class="sub-title active-shape">Need Any Help?</span>
-                    <h3 class="title">Get in Touch With Us</h3>
+
+                    <span class="sub-title active-shape">
+                        Need Any Help?
+                    </span>
+
+                    <h3 class="title">
+                        Get in Touch With Us
+                    </h3>
+
                 </div>
+
 
                 <div class="tj-animate-form d-flex align-items-center">
 
-                    <form class="animate-form" method="POST">
+                    <form
+                        class="animate-form"
+                        method="POST"
+                        action=""
+                    >
 
                         @csrf
+
 
                         <div class="row">
 
                             <!-- First Name -->
                             <div class="col-lg-6 col-md-6">
+
                                 <div class="form__div">
+
                                     <input
                                         type="text"
                                         name="first_name"
@@ -105,13 +177,21 @@
                                         placeholder=" "
                                         required
                                     />
-                                    <label class="form__label">First Name</label>
+
+                                    <label class="form__label">
+                                        First Name
+                                    </label>
+
                                 </div>
+
                             </div>
+
 
                             <!-- Last Name -->
                             <div class="col-lg-6 col-md-6">
+
                                 <div class="form__div">
+
                                     <input
                                         type="text"
                                         name="last_name"
@@ -119,13 +199,21 @@
                                         placeholder=" "
                                         required
                                     />
-                                    <label class="form__label">Last Name</label>
+
+                                    <label class="form__label">
+                                        Last Name
+                                    </label>
+
                                 </div>
+
                             </div>
+
 
                             <!-- Phone -->
                             <div class="col-lg-6 col-md-6">
+
                                 <div class="form__div">
+
                                     <input
                                         type="tel"
                                         name="phone"
@@ -134,13 +222,21 @@
                                         placeholder=" "
                                         required
                                     />
-                                    <label class="form__label">Phone</label>
+
+                                    <label class="form__label">
+                                        Phone
+                                    </label>
+
                                 </div>
+
                             </div>
+
 
                             <!-- Email -->
                             <div class="col-lg-6 col-md-6">
+
                                 <div class="form__div">
+
                                     <input
                                         type="email"
                                         name="email"
@@ -148,46 +244,22 @@
                                         placeholder=" "
                                         required
                                     />
-                                    <label class="form__label">Email Address</label>
+
+                                    <label class="form__label">
+                                        Email Address
+                                    </label>
+
                                 </div>
+
                             </div>
 
-                            <!-- Subject -->
-                            <div class="col-lg-12 col-md-12">
-                                <div class="form__div">
-                                    <input
-                                        type="text"
-                                        name="subject"
-                                        class="form__input"
-                                        placeholder=" "
-                                        required
-                                    />
-                                    <label class="form__label">Subject</label>
-                                </div>
-                            </div>
+                                                        <!-- SMS Consent -->
+                            <div class="col-lg-12 mt-3 mb-4">
 
-                            <!-- Message -->
-                            <div class="col-lg-12">
-                                <div class="form__div">
-                                    <textarea
-                                        name="message"
-                                        class="form__input textarea"
-                                        placeholder=" "
-                                        required
-                                    ></textarea>
-                                    <label class="form__label">Message</label>
-                                </div>
-                            </div>
-
-                            
-                        </div>
-                        <div class="row">
-                            <!-- SMS Consent -->
-                            <div class="col-lg-12 mt-3">
-                                <div class="form-check">
+                                <div class="form-check d-flex align-items-start">
 
                                     <input
-                                        class="form-check-input"
+                                        class="form-check-input flex-shrink-0 mt-1 me-2"
                                         type="checkbox"
                                         name="sms_consent"
                                         id="sms_consent"
@@ -215,10 +287,60 @@
                                     </label>
 
                                 </div>
+
                             </div>
+
+                            <!-- Subject -->
+                            <div class="col-lg-12 col-md-12">
+
+                                <div class="form__div">
+
+                                    <input
+                                        type="text"
+                                        name="subject"
+                                        class="form__input"
+                                        placeholder=" "
+                                        required
+                                    />
+
+                                    <label class="form__label">
+                                        Subject
+                                    </label>
+
+                                </div>
+
+                            </div>
+
+
+                            <!-- Message -->
+                            <div class="col-lg-12">
+
+                                <div class="form__div">
+
+                                    <textarea
+                                        name="message"
+                                        class="form__input textarea"
+                                        placeholder=" "
+                                        required
+                                    ></textarea>
+
+                                    <label class="form__label">
+                                        Message
+                                    </label>
+
+                                </div>
+
+                            </div>
+
+
+
+
                         </div>
+
+
                         <!-- Submit Button -->
-                        <div class="tj-theme-button mt-3">
+                        <div class="tj-theme-button mt-2">
+
                             <button
                                 class="tj-primary-btn contact-btn"
                                 type="submit"
@@ -227,6 +349,7 @@
                                 Send Message
                                 <i class="flaticon-right-1"></i>
                             </button>
+
                         </div>
 
                     </form>
@@ -238,6 +361,7 @@
 
             <!-- Google Map -->
             <div class="col-lg-5">
+
                 <div class="google-map">
 
                     <iframe
@@ -251,13 +375,16 @@
                     </iframe>
 
                 </div>
+
             </div>
 
         </div>
 
     </div>
+
 </section>
 <!--========== Contact Page End ==============-->
+```
 
 
     <!--========== Contact Page End ==============-->
